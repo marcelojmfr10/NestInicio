9ac4e9b62aa2803b8b4a313b782e96a3bf8decb6
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-05-24 10:11:13 -0600

    Bases de nest

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
+    constructor(
+        private readonly carsService: CarsService
+    ){
+
+    }
+
     @Get()
     getAllCars() {
-        return ['toyota', 'honda', 'jeep'];
+        return this.carsService.findAll();
+    }
 
+    @Get(':id')
+    getCarById(@Param('id', ParseIntPipe) id: number) {
+        console.log({id});
+        return this.carsService.findOneById(id);
+    }
+
+    @Post()
+    create(@Body() body: any) {
+        return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() body: any){
+        return body;
+    }
+
+    @Delete(':id')
+    delete(@Param('id', ParseIntPipe) id: number){
+        return {
+            method: 'delete',
+            id
+        }
     }
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+
+    private cars = [
+        {
+            id: 1,
+            brand: 'toyota',
+            model: 'corolla',
+        },
+        {
+            id: 2,
+            brand: 'honda',
+            model: 'civic',
+        },
+        {
+            id: 3,
+            brand: 'jeep',
+            model: 'cherokee',
+        }
+    ];
+
+    findAll() {
+        return this.cars;
+    }
+
+    findOneById(id: number){
+        const car = this.cars.find(car => car.id == id);
+
+        if(!car) throw new NotFoundException(`Car with id ${id} not found`);
+        
+        return car;
+    }
+}
